227ea8fec40adaf816c533de5f1080000f5a8e2a
unknown <delgeoffrey1@gmail.com>
2023-03-20 23:31:24 +0100

    Init bot fonctionnel et première commande réalisée (V2)

--- Handlers/loadCommands.js
+++ Handlers/loadCommands.js
@@ -1,14 +1,3 @@
-// const fs = require('fs')
-
-// module.exports = async bot => {
-//     fs.readdirSync('./Commands').filter(f => f.endsWith('.js')).forEach(async file => {
-//         let command = require(`../Commands/${file}`)
-//         if(!command.name || typeof command.name !== 'string') throw new TypeError(`La commande ${file.slice(0, file.length - 3)} n'a pas de nom`)
-//         bot.commands.set(command.name, command)
-//         console.log(`${file} chargé avec succès`);
-//     })
-// }
-
 const fs = require('fs').promises
 const path = require('path')
 
